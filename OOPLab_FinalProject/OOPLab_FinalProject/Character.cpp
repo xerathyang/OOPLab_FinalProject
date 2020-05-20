7bbfcf8e435b23370d5bce34d880c9f5ce77786a
子賢 楊 <c4940578@gmail.com>
2020-05-20 13:31:37 +0800

    add character implement(not complete

---
+++ OOPLab_FinalProject/OOPLab_FinalProject/Character.cpp
@@ -0,0 +1,13 @@
+#include "Character.h"
+
+using namespace std;
+//in progress
+CharacterSkill::CharacterSkill() {
+	_index = 0;
+	_dex = 0;
+	_action.clear();
+}
+
+CharacterSkill::CharacterSkill(ifstream& fs) {
+
+}
--- OOPLab_FinalProject/OOPLab_FinalProject/Character.h
+++ OOPLab_FinalProject/OOPLab_FinalProject/Character.h
@@ -1,12 +1,36 @@
 #pragma once
-class Character {
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include "Action.h"
 
+using namespace std;
+
+class CharacterSkill {
+public:
+	CharacterSkill();
+	CharacterSkill(ifstream&);
+private:
+	int _index, _dex;
+	vector<Action> _action;
+};
+
+class Character {
+public:
+	Character();
+	Character(ifstream&);
+private:
+	string _name;
+	int _life, _startcard;
 };
 
 class CharacterData {
-
-};
-
-class CharacterSkill {
-
+public:
+	CharacterData();
+	CharacterData(string);
+private:
+	int _charTypecount;
+	vector<Character> _charlist;
 };
